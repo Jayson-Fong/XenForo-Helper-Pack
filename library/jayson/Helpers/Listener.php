4a04818e08530c416f3021327f43c61aafd63920
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-04-14 00:43:07 -0400

    Create Listener.php

---
+++ library/jayson/Helpers/Listener.php
@@ -0,0 +1,9 @@
+<?php
+class jayson_helperPack_Helpers_listener
+{
+        public static function helpers(XenForo_Dependencies_Abstract $dependencies, array $data) {
+                XenForo_Template_Helper_Core::$helperCallbacks += array(
+                        'getidbynameoremail' => array('jayson_helperPack_Helpers_NameEmail', 'helperGetId')
+                );
+        }
+}
