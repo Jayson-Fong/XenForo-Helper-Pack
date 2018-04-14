118edd7c5f1569041b0cb31a9b133271b360fca5
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-04-14 01:09:33 -0400

    Added Support For Id to Username Conversion

--- library/jayson/Helpers/Listener.php
+++ library/jayson/Helpers/Listener.php
@@ -3,7 +3,8 @@ class jayson_helperPack_Helpers_listener
 {
         public static function helpers(XenForo_Dependencies_Abstract $dependencies, array $data) {
                 XenForo_Template_Helper_Core::$helperCallbacks += array(
-                        'getidbynameoremail' => array('jayson_helperPack_Helpers_NameEmail', 'helperGetId')
+                        'getidbynameoremail' => array('jayson_helperPack_Helpers_NameEmail', 'helperGetId'),
+                        'getnamebyid' => array('jayson_helperPack_Helpers_Id', 'helperGetName')
                 );
         }
 }
