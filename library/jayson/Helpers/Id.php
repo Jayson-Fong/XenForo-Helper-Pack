48e72039633c073686ef0e129c6b97fe86055345
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-04-14 01:14:08 -0400

    Set Default Value

--- library/jayson/Helpers/Id.php
+++ library/jayson/Helpers/Id.php
@@ -3,8 +3,13 @@ class jayson_helperPack_Helpers_Id
 {
         public static function helperGetName($id)
         {
-                $userModel = XenForo_Model::create('XenForo_Model_User');
-                $user = $userModel->getUserById($id);
-                return $user["username"];
+                $username = "";
+                if ($id > 0)
+                {
+                        $userModel = XenForo_Model::create('XenForo_Model_User');
+                        $user = $userModel->getUserById($id);
+                        $username = $user["username"];
+                }
+                return $username;
         }
 }
