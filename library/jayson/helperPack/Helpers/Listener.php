1b8378aec33c694874c3f695a45baaa3bac2332c
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-04-26 21:14:31 -0400

    Update Listener.php

--- library/jayson/helperPack/Helpers/Listener.php
+++ library/jayson/helperPack/Helpers/Listener.php
@@ -4,7 +4,8 @@ class jayson_helperPack_Helpers_listener
         public static function helpers(XenForo_Dependencies_Abstract $dependencies, array $data) {
                 XenForo_Template_Helper_Core::$helperCallbacks += array(
                         'getidbynameoremail' => array('jayson_helperPack_Helpers_NameEmail', 'helperGetId'),
-                        'getnamebyid' => array('jayson_helperPack_Helpers_Id', 'helperGetName')
+                        'getnamebyid' => array('jayson_helperPack_Helpers_Id', 'helperGetName'),
+                        'getuserbynameoremail' => array('jayson_helperPack_Helpers_NameEmail', 'helperGetUser')
                 );
         }
 }
