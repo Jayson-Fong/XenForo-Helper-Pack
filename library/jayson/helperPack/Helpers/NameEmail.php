c60497c09ea0c13560baf1ca9f1a15e21873d03c
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-04-26 21:13:59 -0400

    Update NameEmail.php

--- library/jayson/helperPack/Helpers/NameEmail.php
+++ library/jayson/helperPack/Helpers/NameEmail.php
@@ -7,4 +7,9 @@ class jayson_helperPack_Helpers_NameEmail
                 $user = $userModel->getUserByNameOrEmail($name);
                 return $user["user_id"];
         }
+        
+        public static function helperGetUser($name)
+        {
+                return XenForo_Model::create('XenForo_Model_User')->getUserByNameOrEmail($name);
+        }
 }
