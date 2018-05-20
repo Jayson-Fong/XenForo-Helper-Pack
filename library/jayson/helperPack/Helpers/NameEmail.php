3aa2b10597fb222596b8505b6cb5ea42962cddcd
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-05-19 23:41:06 -0400

    Removed Failing Helper

--- library/jayson/helperPack/Helpers/NameEmail.php
+++ library/jayson/helperPack/Helpers/NameEmail.php
@@ -7,9 +7,4 @@ class jayson_helperPack_Helpers_NameEmail
                 $user = $userModel->getUserByNameOrEmail($name);
                 return $user["user_id"];
         }
-        
-        public static function helperGetUser($name)
-        {
-                return XenForo_Model::create('XenForo_Model_User')->getUserByNameOrEmail($name);
-        }
 }
